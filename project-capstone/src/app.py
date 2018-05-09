3ae3963ab299226e9e1b76f22053fe2b43b2bdf7
Philly Lim <phillymllim@gmail.com>
2018-05-09 15:06:39 +0800

    Tortellini beta version done

--- project-capstone/src/app.py
+++ project-capstone/src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from models import random_persons
+from models import random_persons, graph, User
 #request, session, redirect, url_for, 
 #from .models import User, get_todays_recent_posts
 
@@ -11,9 +11,35 @@ def index():
 
 @app.route('/feeling_hungry', methods=['GET'])
 def feeling_hungry():
-    #text_ = "All the random stuff here for User ID: "
+
+    # Randomize a User ID
     user_id = random_persons()
-    return render_template('display.html', user_id=user_id)
+
+    # Initialize the User class with the random user_id selected
+    current_user = User(user_id)
+
+    # Get the top purchases
+    recent_purchases = current_user.get_purchases()
+
+    # Get the product recommendations
+    recommendations = current_user.get_recommendations()
+
+    # Get the novel product recommendations
+    novelty = current_user.get_novelty()
+
+    # Reinitialise another instance/variable of the top purchases
+    purchases = current_user.get_purchases()
+    for row in purchases:
+        product_name = row['product']['name']
+        word_list = product_name.split() # Split the product names into single words
+        recipes = current_user.get_recipes(product_list=word_list) # Get the recipe recommendations here
+
+    return render_template('display.html', 
+            user_id=user_id, 
+            recent_purchases=recent_purchases, 
+            recipes=recipes,
+            novelty=novelty,
+            recommendations=recommendations)
     #return (text_ + str(user_id))
 
 # Remember to remove this before deployment
--- project-capstone/src/models.py
+++ project-capstone/src/models.py
@@ -2,7 +2,7 @@ from py2neo import Graph
 from random import randint
 import os
 
-graph = Graph()
+graph = Graph(bolt=True, host="localhost", http_port=7687, user='neo4j', password='pasta')
 
 def random_persons():
     # Random number generator for User ID since we can't 'login' as a User.
@@ -10,59 +10,81 @@ def random_persons():
     return user_id
 
 class User:
-    def __init__(self, username):
-        self.username = username
+    def __init__(self, user_id):
+        self.user_id = user_id
 
-    def find(self):
-        user = graph.find_one('User', 'username', self.username)
-        return user
-
-    def get_recent_posts(self):
+    def get_purchases(self):
         query = '''
-        MATCH (user:User)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
-        WHERE user.username = {username}
-        RETURN post, COLLECT(tag.name) AS tags
-        ORDER BY post.timestamp DESC LIMIT 5
+        MATCH (u:User)-[b:BOUGHT]->(product:Product)
+        WHERE u.id = {userID}
+        WITH AVG(b.order_total) AS average_orders, b, product
+        WHERE b.order_total >= average_orders
+        RETURN product, b
+        ORDER BY b.order_total DESC
+        LIMIT 10
         '''
 
-        return graph.run(query, username=self.username)
+        return graph.run(query, userID=self.user_id)
 
-    def get_similar_users(self):
-        # Find three users who are most similar to the logged-in user
-        # based on tags they've both blogged about.
+    def get_recommendations(self):
+        # Pearson + KNN recommendations.
         query = '''
-        MATCH (you:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
-              (they:User)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
-        WHERE you.username = {username} AND you <> they
-        WITH they, COLLECT(DISTINCT tag.name) AS tags
-        ORDER BY SIZE(tags) DESC LIMIT 3
-        RETURN they.username AS similar_user, tags
+        MATCH (u1:User {id: {userID}})-[b:BOUGHT]->(rec:Product)
+        WITH u1, avg(b.order_total) AS u1_mean
+
+        MATCH (u1)-[b1:BOUGHT]->(rec:Product)<-[b2:BOUGHT]-(u2)
+        WITH u1, u1_mean, u2, COLLECT({b1: b1, b2: b2}) AS totalorders WHERE size(totalorders) > 10
+
+        MATCH (u2)-[b:BOUGHT]->(rec:Product)
+        WITH u1, u1_mean, u2, avg(b.order_total) AS u2_mean, totalorders
+
+        UNWIND totalorders AS r
+
+        WITH sum( (r.b1.order_total - u1_mean) * (r.b2.order_total - u2_mean) ) AS nom,
+            sqrt( sum( (r.b1.order_total - u1_mean)^2) * sum( (r.b2.order_total - u2_mean) ^2)) AS denom,
+            u1, u2 WHERE denom <> 0
+
+        WITH u1, u2, nom/denom AS pearson
+        ORDER BY pearson DESC LIMIT 10
+
+        MATCH (u2)-[b:BOUGHT]->(rec:Product) WHERE NOT EXISTS( (u1)-[:BOUGHT]->(rec) )
+
+        RETURN rec, SUM(pearson * b.order_total) AS score
+        ORDER BY score DESC 
+        LIMIT 5'''
+
+        return graph.run(query, userID=self.user_id)
+    
+    def get_novelty(self):
+        # Novelty recommendations.
+        query = '''
+        MATCH (user:User {id: {userID}})-[:BOUGHT]->(product)-[:FOUND_IN]->(a:Aisle)-[:IN_CLUSTER]->(cluster)
+        WITH user, cluster, COUNT(*) AS times
+        ORDER BY times ASC
+        LIMIT 1
+        WITH cluster
+        MATCH (cluster)<-[:IN_CLUSTER]-(a)<-[:FOUND_IN]-(p)
+        WITH cluster, a.name AS aisleName, COUNT(p) as numberOfProducts
+        ORDER BY numberOfProducts DESC
+        LIMIT 1
+        WITH aisleName AS x
+        MATCH (Aisle {name: x})<-[:FOUND_IN]-(otherProducts)<-[b:BOUGHT]-()
+        WHERE b.order_total > 10
+        RETURN DISTINCT otherProducts, MAX(b.order_total) AS maxOrders
+        ORDER BY maxOrders DESC
+        LIMIT 5'''
+
+        return graph.run(query, userID=self.user_id)
+
+    def get_recipes(self, product_list):
+        # Get the recipe recommendations
+        query = '''
+        MATCH (c:Category)<-[:TAGGED_AS]-(recipe:Recipe)-[:TAGGED_AS]->(other:Category)
+        WHERE c.name IN {product_list} AND recipe.rating >= 3.0
+        WITH recipe, COLLECT(other.name) AS categories, COUNT(*) AS commonCategories
+        RETURN recipe, categories, commonCategories
+        ORDER BY commonCategories DESC 
+        LIMIT 5
         '''
 
-        return graph.run(query, username=self.username)
-
-    def get_commonality_of_user(self, other):
-        # Find how many of the logged-in user's posts the other user
-        # has liked and which tags they've both blogged about.
-        query = '''
-        MATCH (they:User {username: {they} })
-        MATCH (you:User {username: {you} })
-        OPTIONAL MATCH (they)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag:Tag),
-                       (you)-[:PUBLISHED]->(:Post)<-[:TAGGED]-(tag)
-        RETURN SIZE((they)-[:LIKED]->(:Post)<-[:PUBLISHED]-(you)) AS likes,
-               COLLECT(DISTINCT tag.name) AS tags
-        '''
-
-        return graph.run(query, they=other.username, you=self.username).next
-
-def get_todays_recent_posts():
-    query = '''
-    MATCH (user:User)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
-    WHERE post.date = {today}
-    RETURN user.username AS username, post, COLLECT(tag.name) AS tags
-    ORDER BY post.timestamp DESC LIMIT 5
-    '''
-
-    return graph.run(query, today=date())
-
-
+        return graph.run(query, product_list=product_list)
