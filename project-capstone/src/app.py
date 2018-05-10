7ad7af087c01065873f57d623977bc033b7e2341
Philly Lim <phillymllim@gmail.com>
2018-05-10 11:45:40 +0800

    CSS and Layout work done

--- project-capstone/src/app.py
+++ project-capstone/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, url_for
 from models import random_persons, graph, User
 #request, session, redirect, url_for, 
 #from .models import User, get_todays_recent_posts
